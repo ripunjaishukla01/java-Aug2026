cf6c412ca40b1ceaea9371fb93c5297a46106f38
Ripunjai Shukla <ripunjai79@email.com>
2026-08-13 16:38:15 +0530

    Example

--- Bike.java
+++ Bike.java
@@ -1,19 +1,35 @@
-class Vehicle {
-    int GearsCount;
+// class Vehicle {
+//     int GearsCount;
+      
+//     void Start(){
+//         System.out.println(this);
+// System.out.println(this.GearsCount+ " Vehicle is Starting , ready to go!");
+//     }
+// }
 
-    void Start(){
-System.out.println(" Vehicle is Starting , ready to go!");
-    }
-}
+// public class Bike extends Vehicle {
+//     void Start(){
+//         System.out.println("Bike is Starting , ready to go! ");
+//     }
+//     public static void main(String[] args) {
 
-public class Bike extends Vehicle {
-    void Start(){
-        System.out.println("Bike is Starting , ready to go! ");
-    }
+//         Bike obj = new Bike();
+//         obj.GearsCount = 5;
+//         obj.Start();
+//     }
+// }
+
+
+
+
+
+
+
+public class Bike{
     public static void main(String[] args) {
-
-        Bike obj = new Bike();
-        obj.GearsCount = 5;
-        obj.Start();
+        double value = 10.9;
+     
+        int num =(int) value;
+        System.out.println(num);
     }
 }
